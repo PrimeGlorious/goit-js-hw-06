a54cbc9fbb3e09fc9a6fd7938731ed46731f6137
PrimeGlorious <TheComboPunk@gmail.com>
2023-04-23 21:22:49 +0300

    add fix

--- src/js/task-04.js
+++ src/js/task-04.js
@@ -5,15 +5,15 @@ const decrementBtnEl = document.querySelector('[data-action="decrement"]');
 const incrementBtnEl = document.querySelector('[data-action="increment"]');
 
 decrementBtnEl.addEventListener('click', decrement);
-incrementBtnEl.addEventListener('click', increment)
+incrementBtnEl.addEventListener('click', increment);
 
 
 function decrement() {
   counterValue -= 1;
-  counterValueEl.textContent = counterValue
+  counterValueEl.textContent = counterValue;
 };
 
 function increment() {
   counterValue += 1;
-  counterValueEl.textContent = counterValue
+  counterValueEl.textContent = counterValue;
 };
